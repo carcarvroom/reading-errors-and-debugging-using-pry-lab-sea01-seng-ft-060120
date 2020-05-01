13d346ab25c0de6e3d9abab696cb72077e2d0955
Carly La <carlylynnla@gmail.com>
2020-05-01 04:36:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,2 @@
 # don't forget to add: require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
-
-
-def state_log(date)
-  "Captain's Log, star date #{date}."
-end
-
-def engage
-  date = generate_star_date
-  puts state_log(date)
-end
